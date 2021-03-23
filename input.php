29c782312118e140f12d2a2cacf30d77c3cfcce1
Matthias Wulff <80181235+m-wulffde@users.noreply.github.com>
2021-03-23 13:36:40 +0100

    Update input.php

--- input.php
+++ input.php
@@ -1,6 +1,20 @@
-<?php
-  $Temperatur = $_GET["t"];
-  $datei = fopen("daten.txt","w");
-  echo fwrite($datei,$Temperatur,100);
-  fclose($datei); 
+?php
+// Messwert in Textdatei schreiben
+$Temperatur = $_GET["t"];
+$datei = fopen("daten.txt","w");
+fwrite($datei, $Temperatur,100);
+fclose($datei);
+
+// Daten aufbereiten
+$datum = date("Y-m-d H:i:s");
+
+// Mysqldatenbank öffnen
+$mysqli = new mysqli("localhost", "user", "password", "database");
+
+if ($mysqli->connect_errno) {
+    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+}
+// Messwert in MySQL-Datenbank schreiben
+$sql = "INSERT INTO `messwerte` (`DatumZeit`, `Temp`) VALUES ('$datum', '$Temperatur')";
+$mysqli->query($sql);
 ?>
